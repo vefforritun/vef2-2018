dd8422a5661a490b1d7455738bd1b93ee1b2df34
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-02-01 21:03:40 +0000

    Uppfæra schema.sql fyrir sql injection dæmi

--- daemi/fyrirlestrar/security/schema.sql
+++ daemi/fyrirlestrar/security/schema.sql
@@ -8,4 +8,5 @@ CREATE TABLE students(
   id serial primary key,
   name varchar(64) not null,
   date timestamp with time zone not null default current_timestamp
+  graduated boolean NOT NULL DEFAULT false,
 );
